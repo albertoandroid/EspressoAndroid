d23cd1c0fc0d7ea2b6a5e7f522b27ee5be86c55b
Alberto <info@androiddesdecero.com>
2019-05-15 17:02:35 +0200

    03 continuando

--- app/src/androidTest/java/com/androiddesdecero/espressoudemy/Main2ActivityTest.java
+++ app/src/androidTest/java/com/androiddesdecero/espressoudemy/Main2ActivityTest.java
@@ -8,8 +8,10 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
@@ -22,7 +24,22 @@ public class Main2ActivityTest {
 
     @Test
     public void isDisplayedOnTheScreenTest(){
-        onView(withText("Mi primer Expresso test1"))
+        onView(withText("Mi primer Expresso test"))
                 .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isDisplayedOnTheScreenWithIdTest(){
+        onView(withId(R.id.activityMain2TvMiFirstTest))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void ensureResetWorkTest(){
+        onView(withId(R.id.activityMain2BtReset))
+                .perform(click());
+
+        onView(withId(R.id.activityMain2TvMiFirstTest))
+                .check(matches(withText("Mi primer Expresso test")));
     }
 }
