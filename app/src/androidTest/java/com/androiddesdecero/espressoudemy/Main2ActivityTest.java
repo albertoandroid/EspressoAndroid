bfb751173e62d6137a08715bc2b31982d0b8f0d5
Alberto <info@androiddesdecero.com>
2019-05-15 17:19:03 +0200

    04 mas pruebas

--- app/src/androidTest/java/com/androiddesdecero/espressoudemy/Main2ActivityTest.java
+++ app/src/androidTest/java/com/androiddesdecero/espressoudemy/Main2ActivityTest.java
@@ -4,11 +4,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -40,6 +43,25 @@ public class Main2ActivityTest {
                 .perform(click());
 
         onView(withId(R.id.activityMain2TvMiFirstTest))
-                .check(matches(withText("Mi primer Expresso test")));
+                .check(matches(withText("Reset Texto")));
+    }
+
+    @Test
+    public void ensureChangeEditTextWorkTest(){
+        onView(withId(R.id.activityMain2EtMiFirstEditText))
+                .perform(typeText("Prueba")
+                , ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.activityMain2btChange))
+                .perform(click());
+
+        onView(withId(R.id.activityMain2TvMiFirstTest))
+                .check(matches(withText("Prueba")));
+    }
+
+    @Test
+    public void isDisplayedOnTheScreenErrorTest(){
+        onView(withText("No me muestra"))
+                .check(matches(isDisplayed()));
     }
 }
