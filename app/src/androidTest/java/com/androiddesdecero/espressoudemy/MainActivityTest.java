ae9f0e3e46713d0a7cd240522665bb7f74472e9a
Alberto <info@androiddesdecero.com>
2019-05-15 11:36:49 +0200

    03

--- app/src/androidTest/java/com/androiddesdecero/espressoudemy/MainActivityTest.java
+++ app/src/androidTest/java/com/androiddesdecero/espressoudemy/MainActivityTest.java
@@ -1,7 +1,19 @@
 package com.androiddesdecero.espressoudemy;
 
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
 }
