5aebd4cd68f5f2a4a09dde3647c812433146a7e0
Alberto <info@androiddesdecero.com>
2019-05-15 11:53:02 +0200

    05

--- app/src/androidTest/java/com/androiddesdecero/espressoudemy/MainActivityTest.java
+++ app/src/androidTest/java/com/androiddesdecero/espressoudemy/MainActivityTest.java
@@ -1,5 +1,7 @@
 package com.androiddesdecero.espressoudemy;
 
+import android.widget.TextView;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,7 +11,16 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.core.AllOf.allOf;
+
+import static org.hamcrest.core.StringStartsWith.startsWith;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
@@ -22,6 +33,20 @@ public class MainActivityTest {
     @Test
     public void myFirstTest(){
         onView(withId(R.id.mainActivityTv));
+
+        onView(withText("Prueba Espresso"));
+
+        onView(withContentDescription("elemento TextView"));
+
+        onView(withHint("texto hint"));
+
+        onView(allOf(withText("Prueba Espresso"), withParent(withId(R.id.mainActivityRl))));
+
+        onView(allOf(withId(R.id.mainActivityTv), isDisplayed()));
+
+        onView(withText(startsWith("Prueba")));
+
+        onView(allOf(instanceOf(TextView.class), withId(R.id.mainActivityTv)));
     }
 
 
