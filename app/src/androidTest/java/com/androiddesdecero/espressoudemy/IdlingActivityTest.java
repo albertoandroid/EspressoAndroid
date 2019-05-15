2849342558bd7bd354b89fe08f32326738d51c79
Alberto <info@androiddesdecero.com>
2019-05-15 21:55:54 +0200

    05Configurando

--- app/src/androidTest/java/com/androiddesdecero/espressoudemy/IdlingActivityTest.java
+++ app/src/androidTest/java/com/androiddesdecero/espressoudemy/IdlingActivityTest.java
@@ -1,7 +1,42 @@
 package com.androiddesdecero.espressoudemy;
 
+import com.androiddesdecero.espressoudemy.utils.EspressoIdlingResource;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
 public class IdlingActivityTest {
 
+
+    @Rule
+    public ActivityScenarioRule<IdlingActivity> activityRule =
+            new ActivityScenarioRule<>(IdlingActivity.class);
+
+    @Before
+    public void registerIdlingResource(){
+        IdlingRegistry.getInstance().register(
+                EspressoIdlingResource.getIdlingResource()
+        );
+    }
+    
+
+
+
+
+
+    @After
+    public void unregisterIdlingResource(){
+        IdlingRegistry.getInstance().unregister(
+                EspressoIdlingResource.getIdlingResource()
+        );
+    }
 }
