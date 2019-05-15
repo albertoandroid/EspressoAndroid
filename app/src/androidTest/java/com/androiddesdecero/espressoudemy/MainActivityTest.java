0dd34d9b0f74e79e5a0b63debf6c913736562a4b
Alberto <info@androiddesdecero.com>
2019-05-15 11:42:13 +0200

    04

--- app/src/androidTest/java/com/androiddesdecero/espressoudemy/MainActivityTest.java
+++ app/src/androidTest/java/com/androiddesdecero/espressoudemy/MainActivityTest.java
@@ -1,12 +1,15 @@
 package com.androiddesdecero.espressoudemy;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
@@ -15,5 +18,11 @@ public class MainActivityTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void myFirstTest(){
+        onView(withId(R.id.mainActivityTv));
+    }
+
 
 }
