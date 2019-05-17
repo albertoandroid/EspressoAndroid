1ba1a024f9c0b52583c0d9d5cf538baf493ab606
Alberto <info@androiddesdecero.com>
2019-05-17 17:57:29 +0200

    03

--- app/src/androidTest/java/com/androiddesdecero/espressoudemy/SpinnerActivityTest.java
+++ app/src/androidTest/java/com/androiddesdecero/espressoudemy/SpinnerActivityTest.java
@@ -10,11 +10,16 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.anything;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
@@ -36,5 +41,19 @@ public class SpinnerActivityTest {
 
         onView(withId(R.id.spinnerActivitySp))
                 .check(matches(withSpinnerText(containsString("Manuel"))));
+    }
+
+    @Test
+    public void spinnerTest2(){
+        onView(withId(R.id.spinnerActivitySp))
+                .perform(click());
+
+        onData(
+                allOf(is(instanceOf(String.class)),
+                is("Pablo")))
+                .perform(click());
+
+        onView(withId(R.id.spinnerActivityTv))
+                .check(matches(withText("Monica")));
     }
 }
